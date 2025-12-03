4d6b9036dece6cdd97496335edd157cc48f1d773
Leo Collins <collins.leo@proton.me>
2025-12-03 00:15:23 +0000

    add comments

--- dot_config/nvim/lua/config/keybinds.lua
+++ dot_config/nvim/lua/config/keybinds.lua
@@ -1,3 +1,5 @@
-# Set leader key
+-- Set leader key to spacebar
 vim.g.mapleader = " "
+
+-- Spacebar-cd opens file explorer 
 vim.keymap.set("n", "<leader>cd", vim.cmd.Ex)
--- dot_config/nvim/lua/config/options.lua
+++ dot_config/nvim/lua/config/options.lua
@@ -1,5 +1,10 @@
+-- Line numbers & relative line numbers
 vim.opt.number = true
-vim.opt.cursorline = true
 vim.opt.relativenumber = true
+
+-- Highlight current line
+vim.opt.cursorline = true
+
+-- Tab settings
 vim.opt.shiftwidth = 4
 
